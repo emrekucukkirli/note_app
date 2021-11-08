f9e895914bad2c15f999c4879dfa5e83f4cf5809
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2021-11-08 04:40:49 +0300

    completed home_page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,9 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:note_app/pages/add_note.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,16 +14,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CollectionReference ref = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser!.uid)
+      .collection('notes');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(
+          Navigator.of(context)
+              .push(
             MaterialPageRoute(
               builder: (context) => AddNote(),
             ),
-          );
+          )
+              .then((value) {
+            print("calling set state");
+            setState(() {});
+          });
         },
         child: Icon(
           Icons.add,
@@ -27,12 +42,67 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.blue,
       ),
       appBar: AppBar(
-        title: Text("Notes"),
-        centerTitle: true,
-        backgroundColor: Colors.black54,
+        title: Text(
+          "Notes",
+          style: TextStyle(
+            fontSize: 32,
+          ),
+        ),
+        // centerTitle: true,
+        backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: Text("Notes"),
+      body: FutureBuilder<QuerySnapshot>(
+        future: ref.get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot Map = snapshot.data!.docs[index];
+                DateTime noteDateTime = Map['created'].toDate();
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${Map['title']}",
+                          style: TextStyle(
+                            fontSize: 24,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 10),
+                            child: Text(
+                              DateFormat.yMMMd().add_jm().format(noteDateTime),
+                              style: TextStyle(
+                                  fontSize: 16, color: Colors.white38),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+            // if (snapshot.data!.docs.length == 0) {
+            //   return Center(
+            //     child: Text(
+            //       "Notunuz Bulunmamaktadır",
+            //       style: TextStyle(color: Colors.red),
+            //     ),
+            //   );
+            // }
+          } else {
+            return Center(
+              child: Text("Yükleniyor...."),
+            );
+          }
+        },
       ),
     );
   }
